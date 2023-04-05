6629dabf6d3131010b227ce699a623dbd8e5052c
tahir hussain <tahirhussain2257@gmail.com>
2023-04-05 16:49:51 +0100

    first commit

---
+++ shell_execution.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+script_dir = os.path.dirname(__file__)
+
+script_absolute_path = os.path.join(script_dir + "/script.sh")
+
+subprocess.call(['sh', script_absolute_path])
