977720f44f8c973caf5e7a54908e4778c106c7cb
tahir hussain <tahirhussain2257@gmail.com>
2023-04-06 17:43:07 +0100

    Thursday0604

--- json2yaml/yaml2json.py
+++ json2yaml/yaml2json.py
@@ -2,14 +2,13 @@ import json
 import os
 import sys
 import yaml
-import win32com.client
 
 # check there is file passed
 if len(sys.argv) > 1:
 # opening the file
     if os.path.exists(sys.argv[1]):
         source_file = open(sys.argv[1], "r")
-        source_content = json.load(source_file)
+        source_content = yaml.load(source_file, Loader=yaml.FullLoader)
         source_file.close()
     # if file isn't found
     else:
@@ -19,9 +18,8 @@ else:
     print("Wrong execution format")
 
 # processing the conversion
-output = yaml.dump(source_content)
+output = json.dumps(source_content)
 
 target_file = open(sys.argv[2], "w")
 target_file.write(output)
 target_file.close()
-
